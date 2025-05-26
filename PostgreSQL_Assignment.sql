63865a0e81636ad9bf788b67f872ac978ae03f82
Nadia Mahbuba <nadiamahbuba.webdevsoln@gmail.com>
2025-05-26 10:20:56 +0600

    Update PostgreSQL_Assignment.sql

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -9,6 +9,7 @@ INSERT INTO rangers(name,region) VALUES
 ('Alice Green','Northern Hills'),
 ('Bob White','River Delta'),
 ('Carol King','Mountain Range');
+select * from rangers;
 CREATE TABLE species(
     species_id serial PRIMARY KEY,
     common_name VARCHAR(50) NOT NULL,
@@ -22,7 +23,7 @@ INSERT INTO species(common_name,scientific_name,discovery_date,conservation_stat
 ('Bengal Tiger','Panthera tigris tigris','1758-01-01','Endangered'),
 ('Red Panda','Ailurus fulgens','1825-01-01','Vulnerable'),
 ('Asiatic Elephant',' Elephas maximus indicus','1758-01-01',' Endangered ');
-
+select * from species;
 CREATE TABLE sightings (
     sighting_id SERIAL PRIMARY KEY,
     species_id INT REFERENCES species(species_id),
@@ -36,6 +37,7 @@ INSERT INTO sightings (species_id, ranger_id, location, sighting_time, notes) VA
 (2, 2, 'Bankwood Area', '2024-05-12 16:20:00', 'Juvenile seen'),
 (3, 3, 'Bamboo Grove East', '2024-05-15 09:10:00', 'Feeding observed'),
 (1, 2, 'Snowfall Pass', '2024-05-18 18:30:00',NULL);
+select * from sightings;
 -- Problem 1
 INSERT INTO rangers(name,region) values ('Derek Fox','Coastal Plains');
 --problem 2
